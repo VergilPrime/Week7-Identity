619ece4e710d155e2190a2a6c72ca98b67619c1b
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-02-06 21:58:47 -0800

    Try 2

---
+++ MCMultiverse/Models/ServerPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MCMultiverse.Models
+{
+    public class ServerPage : PreServerPage
+    {
+
+        // Used to determine uptime, uptime is successfulqueries / totalqueries.
+        public long TotalQueries { get; set; }
+        
+        public long SuccessfulQueries { get; set; }
+
+        public long CreatedDate { get; set; }
+
+        public ServerPage()
+        {
+            Create
+        }
+    }
+}
