2e773c211ab35acf6d1cf62597b7fd0d85f40b32
Dustin Mundy <Dustin.A.Mundy@gmail.com>
2018-02-13 13:25:54 -0800

    "Fluent API" (fix for composite key on FavoriteServer associative items in database) (Cannot call Property for the property 'MCServer' on entity type 'FavoriteServer' because it is configured as a navigation property. Property can only be used to configure scalar properties.)

--- MCMultiverse/Data/ApplicationDbContext.cs
+++ MCMultiverse/Data/ApplicationDbContext.cs
@@ -24,6 +24,9 @@ namespace MCMultiverse.Data
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
+            builder.Entity<FavoriteServer>()
+            .HasKey(favorite => new { favorite.UserId, favorite.MCServer });
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
